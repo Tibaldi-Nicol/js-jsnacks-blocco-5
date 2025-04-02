9b571dd63027bd9844e468c1915ab364b45bcd79
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-02 19:31:08 +0200

    [UPD]Add-snack

--- js-jsnacks-blocco-5/snack-6/script.js
+++ js-jsnacks-blocco-5/snack-6/script.js
@@ -12,3 +12,13 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+let zucchineLunghe = zucchine.filter(function(zucchina){
+    return zucchina.length >= 15;
+});
+
+let zucchineCorte = zucchine.filter(function(zucchina){
+    return zucchina.length < 15;
+});
+console.log(zucchineLunghe);
+console.log(zucchineCorte);
--- js-jsnacks-blocco-5/snack-7/script.js
+++ js-jsnacks-blocco-5/snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studentId = 2;
+const student = students.find(function(studente){
+  return studente.id === studentId;
+});
+console.log(student);
